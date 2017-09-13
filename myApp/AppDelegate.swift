6267fe04a86e33c19dcfad344c21b1e365586677
vivek bhandari <vivb@viveks-MacBook-Pro.local>
2017-09-13 15:39:46 -0700

    adding basic support for facebook integration.

--- myApp/AppDelegate.swift
+++ myApp/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreLocation
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,15 +17,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var locationManager: CLLocationManager?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         locationManager = CLLocationManager()
         locationManager?.requestWhenInUseAuthorization()
-        return true
+        return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        FBSDKAppEvents.activateApp()
+    }
+
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
--- myApp/WelcomeViewController.swift
+++ myApp/WelcomeViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import FacebookLogin
+import FBSDKLoginKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
     //MARK: Variables
     var authorization: Authorization? = nil
+    var dict : [String : AnyObject]!
 
     //MARK: Properties
     @IBOutlet weak var usernameText: UITextField!
@@ -37,6 +40,13 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Handle the text field’s user input through delegate callbacks.
         usernameText.delegate = self
         passwordText.delegate = self
+
+        //creating button
+        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+        loginButton.center = view.center
+
+        //adding it to view
+        view.addSubview(loginButton)
     }
 
     override func didReceiveMemoryWarning() {
@@ -94,5 +104,33 @@ class ViewController: UIViewController, UITextFieldDelegate {
             }
         })
         task.resume()
+    }
+
+    //when login button clicked
+    @objc func loginButtonClicked() {
+        let loginManager = LoginManager()
+        loginManager.logIn([ .publicProfile ], viewController: self) { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                self.getFBUserData()
+            }
+        }
+    }
+
+    //function is fetching the user data
+    func getFBUserData(){
+        if((FBSDKAccessToken.current()) != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil){
+                    self.dict = result as! [String : AnyObject]
+                    print(result!)
+                    print(self.dict)
+                }
+            })
+        }
     }
 }
